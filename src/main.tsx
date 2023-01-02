c605047b08ad832bb1b531a5332bd220d8aa5ecb
Darragh Kirwan <darraghuniversal@gmail.com>
2023-01-02 21:12:28 +0000

    Show json formatter by default

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
+        path: '',
+        element: <JsonFormatterPage />,
+      },
+      {
         path: 'json-formatter',
         element: <JsonFormatterPage />,
       },
